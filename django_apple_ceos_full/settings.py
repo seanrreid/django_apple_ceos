1dc362f37e19f8d2a032259fbb0f80b62c4b5bb0
Sean R Reid <sean@torchcodelab.com>
2023-10-16 20:59:24 -0400

    Fixed issues with migrations and broken URLs

--- django_apple_ceos_full/settings.py
+++ django_apple_ceos_full/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'apple_ceos.apps.AppleCeosConfig'
+    'apple_ceos'
 ]
 
 MIDDLEWARE = [
